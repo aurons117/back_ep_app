ff7c3677aa18a215a583cca29b7b9054d8c48f56
Alexis Sandin <icarbajals0600@gmail.com>
2020-10-07 23:43:06 -0500

    S3 connection fixed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('APP_HOST', '0.0.0.0'),
-  port: env.int('NODE_PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ec193d886243c8077d52889e6aa7b487'),
